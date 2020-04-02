be06fec646e299bab73a46261cefefbbe478cb96
BenBrassard94 <62662981+BenBrassard94@users.noreply.github.com>
2020-04-02 15:57:29 -0600

    Added x, y, and z parameters, can't figure out the homogeneous variable yet

--- src/main/java/com/eonsahead/swing/Vector.java
+++ src/main/java/com/eonsahead/swing/Vector.java
@@ -4,4 +4,16 @@ package com.eonsahead.swing;
 
 public class Vector {
     
+    private double x;
+    private double y;
+    private double z;
+   
+    
+    public Vector(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        
+    } // Vector(double, double, double
+    
 } // Vector
