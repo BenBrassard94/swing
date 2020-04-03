7d97d0261f5490e15ae579a3b8a0ef64aa39898b
BenBrassard94 <62662981+BenBrassard94@users.noreply.github.com>
2020-04-03 10:30:07 -0600

    Reset the Vector Class cause I made many mistakes

--- src/main/java/com/eonsahead/swing/Vector.java
+++ src/main/java/com/eonsahead/swing/Vector.java
@@ -4,39 +4,5 @@ package com.eonsahead.swing;
 
 public class Vector {
     
-    private double x;
-    private double y;
-    private double z;
-   
-    
-    public Vector(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        
-    } // Vector(double, double, double
-    
-    public double get( int value) {
-        value = 0;
-        if (value == 0){
-            return this.x;
-        } // if
-        else if (value == 1){
-            return this.y;
-        } // else if
-        else if (value == 2){
-            return this.z;
-        } // else if
-        // else (value == 3){
-        
-       // } // else
-       return value;
-    } // get(int)
-    
-        public double dot( Vector v ) {
-        double xP = this.x * v.x;
-        double yP = this.y * v.y;
-        double zP = this.z * v.z;
-        return xP + yP + zP;
-    } // dot( Vector2D )
+
 } // Vector
