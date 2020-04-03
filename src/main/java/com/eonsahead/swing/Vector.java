9d5f6de5b2cb7665fcf16d6ad2cebe95541af3ea
BenBrassard94 <62662981+BenBrassard94@users.noreply.github.com>
2020-04-02 18:51:13 -0600

    Added dot class

--- src/main/java/com/eonsahead/swing/Vector.java
+++ src/main/java/com/eonsahead/swing/Vector.java
@@ -16,4 +16,27 @@ public class Vector {
         
     } // Vector(double, double, double
     
+    public double get( int value) {
+        value = 0;
+        if (value == 0){
+            return this.x;
+        } // if
+        else if (value == 1){
+            return this.y;
+        } // else if
+        else if (value == 2){
+            return this.z;
+        } // else if
+        // else (value == 3){
+        
+       // } // else
+       return value;
+    } // get(int)
+    
+        public double dot( Vector v ) {
+        double xP = this.x * v.x;
+        double yP = this.y * v.y;
+        double zP = this.z * v.z;
+        return xP + yP + zP;
+    } // dot( Vector2D )
 } // Vector
