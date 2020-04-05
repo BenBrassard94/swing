101af9dac9754c1d54e566b6354af593627061a8
BenBrassard94 <62662981+BenBrassard94@users.noreply.github.com>
2020-04-05 16:42:18 -0600

    Added javadoc comments

--- src/main/java/com/eonsahead/swing/Vector.java
+++ src/main/java/com/eonsahead/swing/Vector.java
@@ -1,12 +1,33 @@
 package com.eonsahead.swing;
 
+/**
+ * Models a 4 component vector that will be used to model a 3D 
+ * object.
+ * 
+ * @author bj.brassard
+ * @version 5 April 2020
+ */
+
 public class Vector {
 
+    /**
+     * Each  variable is self explanatory but h is the homogeneous 
+     */
+    
     private double x;
     private double y;
     private double z;
     private double h;
 
+    /**
+     * Constructor that sets x, y, and z components to the vector
+     * The homogeneous is set to 1.
+     * 
+     * @param x The x component of the vector
+     * @param y The y component of the vector
+     * @param z The z component of the vector
+     */
+    
     public Vector(double x, double y, double z) {
         this.x = x;
         this.y = y;
@@ -15,6 +36,15 @@ public class Vector {
 
     } // Vector(double, double, double)
 
+    /**
+     * Constructor that accepts all four parameters of the vector
+     * 
+     * @param x The x component of the vector
+     * @param y The y component of the vector
+     * @param z The z component of the vector
+     * @param h The homogeneous component of the vector
+     */
+    
     public Vector(double x, double y, double z, double h) {
         this.x = x;
         this.y = y;
@@ -23,6 +53,13 @@ public class Vector {
 
     } // Vector(double, double, double, double)
 
+    /**
+     * Returns the given component of the vector
+     * 
+     * @param num The component that will be returned
+     * @return The vector component
+     */
+    
     public double get(int num) {
         switch (num) {
             case 0:
@@ -39,6 +76,13 @@ public class Vector {
         } // switch
     } // get()
 
+    /**
+     * Multiply components of the vector with another vector
+     * 
+     * @param v The other vector
+     * @return The dot product
+     */
+    
     public double dot(Vector v) {
         double xP = this.get(0) * v.get(0);
         double yP = this.get(1) * v.get(1);
@@ -48,15 +92,34 @@ public class Vector {
 
     } // dot(Vector)
 
+    /**
+     * Returns the magnitude of the given vector
+     * 
+     * @return The magnitude of the vector
+     */
+    
     public double magnitude() {
         return Math.sqrt(this.dot(this));
     } // magnitude()
 
+    /**
+     * Normalizes the vector
+     * 
+     * @return The normalized vector
+     */
+    
     public Vector normalize() {
         double m = this.magnitude();
         return new Vector(this.get(0) / m, this.get(1) / m, this.get(2) / m,
                 this.get(3) / m);
     } // normalize()
+    
+    /** 
+     * Cross product between two vectors
+     * 
+     * @param v The other vector
+     * @return The cross multiplied vectors
+     */
     
     public Vector cross(Vector v){
         double xC = this.get(1) * v.get(2) - this.get(2) * v.get(1);
